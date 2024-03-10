5462cf52861841bfcd4be762d5bb5dec0d8bb04f
Oriana Andrea Soliz Lara <108158939+Orianasoliz35@users.noreply.github.com>
2024-03-10 03:43:38 -0400

    Create Entry.cs

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,18 @@
+using System;
+//Entry file - Week 2
+public class Entry 
+{
+    public string DateCreated {get;}
+    public string Content {get;}
+    public Entry (string dateInput, string content) 
+    {
+        DateCreated = dateInput;
+        Content = content;
+    }
+    public void DisplayEntry()
+    {
+        Console.WriteLine($"Date: {DateCreated}");
+        Console.WriteLine($"Content: {Content}\n");
+    }
+
+}
