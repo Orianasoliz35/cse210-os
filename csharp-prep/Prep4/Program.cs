c2b2a69a891ca3ffe374963d89153869fac2db01
Oriana Andrea Soliz Lara <108158939+Orianasoliz35@users.noreply.github.com>
2024-03-10 00:58:45 -0400

    Update Program.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,47 +1,41 @@
 using System;
 using System.Collections.Generic;
 
-class Program
+List<int> numbers = new List<int>();
+
+int userNumber = -1;
+while (userNumber != 0)
 {
-    static void Main(string[] args)
+    Console.Write("Enter a number (0 to quit): ");
+
+    string userResponse = Console.ReadLine();
+    userNumber = int.Parse (userResponse);
+
+    if (userNumber != 0)
     {
-        List<int>  numbers = new List<int>();
-
-        int useNumber = -1;
-        while (userNumber != 0)
-        {
-            Console.Write("Enter a number (0 to quit): ");
-
-            string userResponse = Console.ReadLine();
-            userNumber = int.Parse(userNumber);
-
-            if (userNumber != 0)
-            {
-               numbers.Add(userNumber);
-            }
-        }
-     //Part 1: Compute the sum
-        int sum = 0;
-        foreach (int number in numbers)
-        {
-            sum += number;
-        }
-         
-         Console.WriteLine($"The sum is {sum}");
-    // Part 2: Compute the average
-         float average = ((float)sum) / numbers.Count;
-         Console.WriteLine($"The average is: {average}");
-    //Part 3: Find the max
-        int max = numbers [0];
-
-        foreach (int number in numbers)
-        {
-            if (number > max)
-            {
-                max = number;
-            }
-        }
-
-        Console.WriteLine($"The max is: {max}");
+        numbers.Add(userNumber);
     }
 }
+//Part 1: Compute the sum
+int sum = 0;
+foreach (int number in numbers)
+{
+    sum += number;
+}
+
+Console.WriteLine($"The sum is {sum}");
+// Part 2: Compute the average
+float average = ((float)sum) / numbers.Count;
+Console.WriteLine($"The average is: {average}");
+//Part 3: Find the max
+int max = numbers[0];
+
+foreach (int number in numbers)
+{
+    if (number > max)
+    {
+        max = number;
+    }
+}
+
+Console.WriteLine($"The max is: {max}");
