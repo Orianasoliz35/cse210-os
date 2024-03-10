7783af1e9fce3a71321f495cfdeae47fe705e40a
Oriana Andrea Soliz Lara <108158939+Orianasoliz35@users.noreply.github.com>
2024-03-10 03:42:30 -0400

    Update Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,49 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop02 World!");
+{
+        Journal journal = new Journal();
+
+        Console.WriteLine("Welcome to Your Daily Journal!");
+    
+        while (true) {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Add Entry");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            Console.Write("Please select an option (1-5) ");
+            string choice = Console.ReadLine();
+
+            switch (choice) 
+            {
+                case "1":
+                journal.AddEntry();
+                break;
+
+                case "2":
+                journal.DisplayEntries();
+                break;
+
+                case "3":
+                journal.LoadFromFile();
+                break;
+
+                case "4":
+                journal.SaveToFile();
+                break;
+
+                case "5":
+                Console.WriteLine("Goodbye!");
+                return;
+
+                default:
+                Console.WriteLine("Invalid selection, please enter a number 1-5.");
+                break;
+            }
+        }
     }
 }
